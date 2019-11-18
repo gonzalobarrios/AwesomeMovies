29c05a6d40174b59daa34b9d1733184dea172899
Joaquin Artola <artolajoaquin93@gmail.com>
2019-11-18 20:37:05 -0300

    super mega integration test

--- app/src/test/java/com/example/awesomemovies/MoviesDataTest.kt
+++ app/src/test/java/com/example/awesomemovies/MoviesDataTest.kt
@@ -6,39 +6,63 @@ import com.barriosartola.awesomeapp.data.repository.notes.DatabaseNotesDataStore
 import com.barriosartola.awesomeapp.inject.movieGenreModule
 import com.barriosartola.awesomeapp.mocks.NetworkingManagerMock
 import com.barriosartola.awesomeapp.mocks.NotesDataStoreFactoryMock
+import com.example.awesomemovies.data.dao.GenreDao
 import com.example.awesomemovies.data.dao.MovieDao
 import com.example.awesomemovies.data.dao.MovieGenreJoinDao
+import com.example.awesomemovies.data.repository.GenreSourceDataRepository
+import com.example.awesomemovies.data.repository.genres.DatabaseGenreDataStore
+import com.example.awesomemovies.data.repository.genres.GenreDataStore
 import com.example.awesomemovies.data.repository.moviegenrejoin.DatabaseMovieGenreDataStore
 import com.example.awesomemovies.data.repository.movies.CloudMoviesDataStore
 import com.example.awesomemovies.data.repository.movies.DatabaseMovieDataStore
 import com.example.awesomemovies.data.repository.movies.MoviesDataStoreFactory
 import com.example.awesomemovies.data.service.MovieService
+import com.example.awesomemovies.mocks.GenreDaoMock
 import com.example.awesomemovies.mocks.MovieDaoMock
 import com.example.awesomemovies.mocks.MovieGenreDaoMock
 import com.example.awesomemovies.mocks.MoviesServiceMock
+import kotlinx.coroutines.*
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.runners.MockitoJUnitRunner
+import kotlin.coroutines.CoroutineContext
 
 @RunWith(MockitoJUnitRunner::class)
-class MoviesDataTest {
+class MoviesDataTes: CoroutineScope  {
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main
+
     @Mock
     private lateinit var mockContext: Context
+
+
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
 
     lateinit var networkingManagerMock: NetworkingManagerMock
     lateinit var movieDao: MovieDao
     lateinit var movieService: MovieService
     lateinit var movieGenreDataStore: DatabaseMovieGenreDataStore
+    lateinit var genreDataStore: DatabaseGenreDataStore
+    lateinit var genreDao: GenreDao
+    lateinit var genreSourceDataRepository: GenreSourceDataRepository
     lateinit var movieGenreJoinDao: MovieGenreJoinDao
     lateinit var moviesDataStoreFactory: MoviesDataStoreFactory
 
     @Before
     fun beforeEachTest(){
+        Dispatchers.setMain(mainThreadSurrogate)
+
         movieService = MoviesServiceMock()
         networkingManagerMock = NetworkingManagerMock(mockContext)
         movieDao = MovieDaoMock()
+        genreDao = GenreDaoMock()
+        genreDataStore = DatabaseGenreDataStore(genreDao)
+        genreSourceDataRepository = GenreSourceDataRepository(genreDataStore)
         movieGenreJoinDao = MovieGenreDaoMock()
         movieGenreDataStore = DatabaseMovieGenreDataStore(movieGenreJoinDao)
         moviesDataStoreFactory = MoviesDataStoreFactory(movieService, movieDao, networkingManagerMock, movieGenreDataStore )
@@ -62,5 +86,20 @@ class MoviesDataTest {
     fun testNetworking() {
         val networkingManager = NetworkingManagerMock(mockContext)
         assert(networkingManager.isNetworkOnline())
+    }
+
+    @Test
+    fun genreSourceGetMockDataCorrectly(){ runBlocking {
+        launch(Dispatchers.Main) {
+            val genre = genreSourceDataRepository.getGenre(7)
+            assert(genre.id == 7)
+            assert(genre.name == "El genre papa")        }
+    }
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain() // reset main dispatcher to the original Main dispatcher
+        mainThreadSurrogate.close()
     }
 }
